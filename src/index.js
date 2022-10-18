d73d1ffbb4ca064237fca6dee3086ad875142bd6
tejasvijb <jbtejasvi@gmail.com>
2022-10-18 21:08:51 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Video } from './Video';
+import { Menu } from './Menu';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const VIDEOS = {
+  fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
+  slow: 'https://content.codecademy.com/courses/React/react_video-slow.mp4',
+  cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
+  eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { src: VIDEOS.fast };
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Video Player</h1>
+        <Menu />
+        <Video />
+      </div>
+    );
+  }
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <App />
